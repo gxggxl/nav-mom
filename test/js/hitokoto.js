f04a9447dfc647576cea596b23e37b09544b9f24
gxggxl <gxggxl@qq.com>
2020-03-17 15:46:54 +0800

    :+1:

--- test/js/hitokoto.js
+++ test/js/hitokoto.js
@@ -1,16 +1,19 @@
 function getHitokoto() {
-$.ajax({
-        url: "https://api.imjad.cn/hitokoto/?encode=jsc&charset=utf-8&length=50",
-        dataType: "jsonp",
-        async: true,
-      jsonp: "callback",
-      jsonpCallback: "hitokoto",
-        success: function(result) {
-  $('#hitokoto').html("<p>" + result.hitokoto + "</p>")
-        },
-        error: function() {
-  $('#hitokoto').html("<p>读取数据失败了的说……_(:з」∠)_</p>")
-        }
-         });
- }
-getHitokoto()
+  $.ajax({
+    url: "https://api.imjad.cn/hitokoto/?encode=jsc&charset=utf-8&length=50",
+    dataType: "jsonp",
+    async: true,
+    jsonp: "callback",
+    jsonpCallback: "hitokoto",
+    success: function(result) {
+      $("#hitokoto").html("<p>" + result.hitokoto + "</p>");
+    },
+    error: function() {
+      $("#hitokoto").html("<p>读取数据失败了的说……_(:з」∠)_</p>");
+    }
+  });
+}
+getHitokoto();
+
+var date = new Date().getFullYear();
+document.getElementById("year").innerHTML = date;
