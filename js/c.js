8ea3ff68fad253202e344a425c876c02709406af
gxggxl <gxggxl@qq.com>
2020-07-21 22:09:55 +0800

    Update c.js

--- js/c.js
+++ js/c.js
@@ -1,3 +1,10 @@
+/*
+ * @Author       : gxggxl
+ * @E-mail       : gxggxl@qq.com
+ * @Date         : 2020-03-27 13:07:11
+ * @LastEditTime : 2020-07-21 22:09:21
+ * @FilePath     : /nav-page/js/c.js
+ */ 
 if ("addEventListener" in window) {
   window.addEventListener("load", function() {
     document.body.className = document.body.className.replace(
